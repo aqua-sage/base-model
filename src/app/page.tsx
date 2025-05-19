3a9c4845cadc5ff551d2fd702048d98845a44f8e
pratham-19 <prathamsinghal2003@gmail.com>
2025-05-19 15:13:20 +0530

    fix: type issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { ThemeToggle } from "@/components/buttons/theme-toggle";
-import Image from "next/image";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
